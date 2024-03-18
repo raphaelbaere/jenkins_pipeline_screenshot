c4ac591d4146813d5c9235631948f6cba9ad6a0e
Raphael Baere de Sá Oliveira <raphaelbaere@id.uff.br>
2024-03-18 03:34:17 -0300

    Initial commit

--- capture.js
+++ capture.js
@@ -3,8 +3,10 @@ const { ImgurClient } = require('imgur');
 const fs = require('fs');
 
 const buildNumber = process.argv[2];
+const jobName = process.argv[3];
+const clientIdImgur = process.argv[4];
 
-const client = new ImgurClient({ clientId: "a355bf1c455a135" }); // Substitua pelo seu Client ID do Imgur
+const client = new ImgurClient({ clientId: clientIdImgur }); // Substitua pelo seu Client ID do Imgur
 
 async function captureScreenshotAndUpload() {
     const browser = await puppeteer.launch();
@@ -12,11 +14,11 @@ async function captureScreenshotAndUpload() {
     await page.goto('http://localhost:8080/login'); // Página de login do Jenkins
     
     // Preencher formulário de login
-    await page.type('#j_username', 'raphaelbaere'); // Substitua 'seu-usuario' pelo nome de usuário do Jenkins
-    await page.type('#j_password', '1598746230A'); // Substitua 'sua-senha' pela senha do Jenkins
+    await page.type('#j_username', 'admin'); // Substitua 'seu-usuario' pelo nome de usuário do Jenkins
+    await page.type('#j_password', 'admin'); // Substitua 'sua-senha' pela senha do Jenkins
     await page.click('form[name="login"] > button[type="submit"]'); // Enviar formulário de login
 
-    await page.goto(`http://localhost:8080/job/chronos-qa-api-pipeline/${buildNumber}/allure/`);
+    await page.goto(`http://localhost:8080/job/${jobName}/${buildNumber}/allure/`);
     // Ir para a página do relatório Allure após o login
     await page.setViewport({
         width: 1920, // Largura da tela
