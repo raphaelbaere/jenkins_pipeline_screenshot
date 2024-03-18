b0bb5b50357adf5852a587112efa62e4735895da
Raphael Baere de Sá Oliveira <raphaelbaere@id.uff.br>
2024-03-18 05:41:48 -0300

    fix: mudei

--- capture.js
+++ capture.js
@@ -1,14 +1,15 @@
 const puppeteer = require('puppeteer');
-const { ImgurClient } = require('imgur');
 const fs = require('fs');
+const { WebhookClient } = require('discord.js');
 
-const buildNumber = process.argv[2];
-const jobName = process.argv[3];
-const clientIdImgur = process.argv[4];
+const jobName = process.argv[2];
+const buildNumber = process.argv[3];
+const buildResult = process.argv[4];
+const message = process.argv[5];
+const webHook = process.argv[6];
 
-const client = new ImgurClient({ clientId: clientIdImgur }); // Substitua pelo seu Client ID do Imgur
 
-async function captureScreenshotAndUpload() {
+async function captureScreenshotAndSend() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('http://localhost:8080/login'); // Página de login do Jenkins
@@ -28,16 +29,45 @@ async function captureScreenshotAndUpload() {
     // Capturar a screenshot da página
     await page.screenshot({ path: 'screenshot.png' });
 
-    // Fazer upload da captura de tela para o Imgur
-    const response = await client.upload({
-        image: fs.createReadStream('screenshot.png'),
-        type: 'stream',
-      });
-      console.log(response.data.link);
+    const webhook = new WebhookClient({ url: `${webHook}` });
+
+    let color
+
+    switch (buildResult) {
+        case 'SUCCESS':
+            color = 65280;
+            break;
+        case 'FAILURE':
+            color = 16711680;
+            break;
+        case 'UNSTABLE':
+            color = 16744192;
+            break;
+        case 'ABORTED':
+            color = 8421504;
+            break;
+        default: 
+            color = 16711680;
+            break;
+    }
+
+    await webhook.send({
+        username: "Jenkins",
+        avatar_url: "https://i.imgur.com/oBPXx0D.png",
+        files: [{
+            attachment: './screenshot.png',
+            name: 'screenshot.png'
+        }],
+        embeds: [{
+            description: `${message}`,
+            color,
+            image:{ url:"attachment://screenshot.png"},
+          }]
+    });
+
 
     await browser.close();
 
-    return response.data.link;
+    return;
 }
-
-captureScreenshotAndUpload();
+captureScreenshotAndSend();
